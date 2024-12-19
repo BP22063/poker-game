22adb9d2a7836ecfca34387308c0d186ef9e267d
BP22063 <bp22063@shibaura-it.ac.jp>
2024-12-19 22:15:08 +0900

    sampleを写経

---
+++ sample/sever/SampleMessage.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SampleMessage {
+    int id;
+    String password;
+    String message;
+
+    public SampleMessage(int id, String password, String message) {
+        this.id =id;
+        this.password = password;
+        this.message = message;
+    }
+}
